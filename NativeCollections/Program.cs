9b2a08ae3e713057907b0c00eb02ffe246b01814
thatavieira <thagurgel0909@gmail.com>
2023-01-13 17:04:26 -0300

    adc program file

---
+++ NativeCollections/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NativeCollections;
+
+class Exercise
+{
+    public static void Main(string[] args)
+    {
+        var exercise01 = new AlunosAcimaDaMedia();
+        exercise01.Execute();
+    }
+}
